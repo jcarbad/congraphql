f6a37c123e5a9e5a5f1b7ab9edae6395d6152921
Armando Carballo <armando.carballo@epicsoftwaredev.com>
2018-08-06 17:03:22 -0600

    Section 4, lesson 19

---
+++ users/schema/schema.js
@@ -0,0 +1,54 @@
+const graphql = require('graphql')
+const axios = require('axios')
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema
+} = graphql
+
+// Schema definitions
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString }
+	}
+});
+
+const UserType = new GraphQLObjectType({
+	name: 'User',
+	fields: {
+		id: { type: GraphQLString },
+		firstName: { type: GraphQLString },
+		age: { type: GraphQLInt	},
+		company: {
+			type: CompanyType,
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${ parentValue.companyId }`)
+				  .then (res => res.data)
+			}
+		}
+	}
+});
+
+// Root Query: entry point to the graph of data
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		user: {
+			type: UserType,
+			args: {
+				id: { type: GraphQLString }
+			},
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/users/${ args.id }`).then(res => res.data);
+			}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
+});
